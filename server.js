6b5c96d89cdd71f464e1c0aa694fb2fa71539e3d
Gedion Adamu <gedionadamu@gmail.com>
2022-06-15 11:33:33 -0400

    updated routes

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => res.sendFile('index.html'));
+app.get('*', (req, res) => res.sendFile('./public/index.html'));
 
-app.get('/notes', (req,res)=> res.sendFile('notes.html'));
+app.get('/notes', (req,res)=> res.sendFile('./public/notes.html'));
 
 app.listen(PORT, () => 
 console.log(`App listening at http;//localhost:${PORT}`))
