aab57680586b19c0899f4c4798a0efd232b5e2c8
Gedion Adamu <gedionadamu@gmail.com>
2022-06-18 17:42:15 -0400

    updated Read Me file.

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# Notetaker
-A Simple app to take notes 
+
+
+  # Note Taker
+
+  [MIT](https://mit-license.org/)   
+  ![MIT](https://img.shields.io/badge/license-MIT-green.svg)  
+
+## Discription
+
+  This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+
+# Table of contents
+
+1. [Installation](#Installation)
+2. [How to Use](#How-To-Use.)
+3. [License](#License)
+4. [Contributing Guidelines](#Contrubuting-Guidelines)  
+5. [Tests](#Tests)
+6. [Questions](#Questions)
+
+# 1. Installation
+
+  npm express , npm path, npm fs, npm crypto
+
+# How To Use.
+
+To install necessary dependencies, run the following command:
+  User is presented with existing notes listed in the left-hand column, plus empty feilds to enter a new note title and the note text in the right hand columns, when hiting the save icon it will save the note and places it in the left side as saved notes. 
+
+# License
+
+  ## This Project is licensed under MIT license.
+
+  
+
+# Contrubuting Guidelines
+
+  None
+
+# ScreenShot
+![note empty feild](./public/assets/screenshots/Screen%20Shot%202022-06-18%20at%205.35.57%20PM.png)
+![note saved feild](./public/assets/screenshots/Screen%20Shot%202022-06-18%20at%205.36.18%20PM.png)
+# Tests
+
+To run test, run the following comands :-  
+
+   None
+
+
+# Questions
+
+  If you have any question please contact me using the following :-   
+  Github :- https://github.com/Gedionadamu/Notetaker   
+  Email:- gedionadamu@gmail.com
+
--- server.js
+++ server.js
@@ -1,15 +1,67 @@
+//imports all required modules
 const express = require('express');
 const path = require('path');
+const db = require('./db/db.json')
+const fs = require('fs');
+const crypto = require('crypto');
+
 
 const app = express();
+// selects process port
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
 
-app.get('*', (req, res) => res.sendFile('./public/index.html'));
+app.use(express.json());
+app.use(express.static('public'));
+// creates route to index.html
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'public/index.html')));
+//creates route to index.html
+app.get('/notes', (req,res)=> res.sendFile(path.join(__dirname,'public/notes.html')));
+// gets thejson data base
+app.get('/api/notes',(req,res) => res.json(db));
+//posts to the notes route and updates the data base and rendered html
+app.post('/api/notes',(req,res)=> {
+    console.info(req.body)
+    // deconstructs the body from the post 
+    const {title, text } = req.body;
+    if(title && text){
+        const newnote = {
+            title,
+            text, 
+            //creates unique ID for every note
+            id : crypto.randomUUID()
+        };
+        // updated the db variable with newly created note
+        db.push(newnote)
+        // reads and updated the updates the JSON file.
+        fs.readFile('./db/db.json', "utf8",(err,data) => {
+           let mytext = JSON.parse(data)
+           mytext.push(newnote)
+           
+           const noteString = JSON.stringify(mytext);
+           fs.writeFile('./db/db.json', noteString,  (err) =>
+           err
+             ? console.error(err)
+             : console.log(
+                 `note has been written to JSON file`
+               ) );
+   
+           const response = {
+               status : 'success',
+               body : newnote,
+           };
+           res.status(201).json(response);
+        });
+        
+    }
+    else {
+        res.status(400).json('note does not contain anything.')
+    }
+});
+
+
 
 app.get('/notes', (req,res)=> res.sendFile('./public/notes.html'));
-
 app.listen(PORT, () => 
 console.log(`App listening at http;//localhost:${PORT}`))
 ;
